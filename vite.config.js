34c1920b5a226bbef2362436352d4184261d39b6
yangbingrui <1005401496@qq.com>
2024-10-01 17:35:10 +0800

    dev:

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/examples/bun/",
+  base: "/examples/my-bun/",
   assetsInclude: ["**/*.glb"],
 });
